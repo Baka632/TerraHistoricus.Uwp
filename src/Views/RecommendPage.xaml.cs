5487cb80182bccea576500fe87ece9d0016dde96
Baka632 <stevemc123456@outlook.com>
2024-05-26 14:26:27 +0800

    调整推荐页里大图图像的容器控件，鼠标指向图像时现在会更改其光标了

--- src/Views/RecommendPage.xaml.cs
+++ src/Views/RecommendPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
+using Windows.UI.Core;
+
 namespace TerraHistoricus.Uwp.Views;
 
 /// <summary>
@@ -7,6 +9,8 @@ namespace TerraHistoricus.Uwp.Views;
 /// </summary>
 public sealed partial class RecommendPage : Page
 {
+    private readonly CoreCursor buttonCursor = new(CoreCursorType.Hand, 0);
+    private CoreCursor cursorBeforePointerEntered = null;
     public RecommendViewModel ViewModel { get; } = new RecommendViewModel();
 
     public RecommendPage()
@@ -22,11 +26,16 @@ public sealed partial class RecommendPage : Page
 
     private void OnRecommendComicGridPointerEntered(object sender, PointerRoutedEventArgs e)
     {
+        cursorBeforePointerEntered = Window.Current.CoreWindow.PointerCursor;
+        Window.Current.CoreWindow.PointerCursor = buttonCursor;
+
         ShowRecommendInfoStoryboard.Begin();
     }
 
     private void OnRecommendComicGridPointerExited(object sender, PointerRoutedEventArgs e)
     {
+        Window.Current.CoreWindow.PointerCursor = cursorBeforePointerEntered;
+
         HideRecommendInfoStoryboard.Begin();
     }
 }
