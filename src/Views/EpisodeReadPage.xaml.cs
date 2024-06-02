fdafcb3f305d77b687840fed3e671cbe2dd279a7
Baka632 <stevemc123456@outlook.com>
2024-06-03 00:09:16 +0800

    进一步完善漫画阅读页的缩放功能，优化底部控制项目的布局

--- src/Views/EpisodeReadPage.xaml.cs
+++ src/Views/EpisodeReadPage.xaml.cs
@@ -1,14 +1,19 @@
 ﻿// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
 using Microsoft.Toolkit.Uwp.UI.Controls;
+using Microsoft.Toolkit.Uwp.UI.Extensions;
 
 namespace TerraHistoricus.Uwp.Views;
 
+[INotifyPropertyChanged]
 /// <summary>
 /// 可用于自身或导航至 Frame 内部的空白页。
 /// </summary>
 public sealed partial class EpisodeReadPage : Page
 {
+    [ObservableProperty]
+    private ScrollViewer listViewerScrollViewer;
+
     public EpisodeReadViewModel ViewModel { get; } = new EpisodeReadViewModel();
 
     public EpisodeReadPage()
@@ -79,6 +84,17 @@ public sealed partial class EpisodeReadPage : Page
         if (grid.DataContext is PageDetailWrapper wrapper && wrapper.IsSuccess != true)
         {
             await wrapper.TryInitialize();
+        }
+    }
+
+    private void OnComicImageListLoaded(object sender, RoutedEventArgs e)
+    {
+        ScrollViewer listviewScrollViewer = ComicImageList.FindDescendant<ScrollViewer>();
+        if (listviewScrollViewer is not null)
+        {
+            listviewScrollViewer.MinZoomFactor = 0.5f;
+            listviewScrollViewer.MaxZoomFactor = 2f;
+            ListViewerScrollViewer = listviewScrollViewer;
         }
     }
 }
